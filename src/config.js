d930c92e4b684e0cc5704dc440f1544b9e17f8ef
mdaboo10 <58439831+Daboo-mohesh@users.noreply.github.com>
2023-02-20 11:35:29 +0400

    config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // basename: only at build time to set, and don't add '/' at end off BASENAME for breadcrumbs,  like '/berry-material-react/react/default'
-    basename: '/SuperLeague',
+    basename: '/superleague',
     defaultPath: '/home',
     fontFamily: `'Roboto', sans-serif`,
     borderRadius: 12,
